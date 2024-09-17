c1fbf9a2da19bd215636c02c4cbf2b084d0aa89e
Alexandre <alex301097@hotmail.fr>
2024-09-17 22:33:11 +0200

    POST test

--- Prediction_api.py
+++ Prediction_api.py
@@ -45,16 +45,14 @@ def info_client(ID_CLIENT: int):
    INFO_CLIENT_dict = INFO_CLIENT.to_dict()
    return INFO_CLIENT_dict
 
-@app.get("/INFO_CLIENTS_GLOBAL")
+@app.post("/INFO_CLIENTS_GLOBAL")
 def info_client_global():
     # Ajouter une colonne 'ID' basée sur l'index
     Base_client['ID'] = Base_client.index
     # Convertir en dictionnaire, avec l'index comme ID
-    INFO_CLIENT_global_dict = Base_client.to_dict
+    INFO_CLIENT_global_dict = Base_client.to_dict(orient="records")
     
     return INFO_CLIENT_global_dict
-
-info_client_global()
 
 
 @app.get("/predict")
